473eff6b5d8a818c4ef6b24e6d9e5e4aba5cd7a1
Steven Belowsky <steven.belowsky@knights.ucf.edu>
2014-01-26 07:51:39 -0500

    No ones ahead of me!
    Characters almost fight now.
    -Steven

---
+++ Assets/Scripts/DestroyExtraneousClient.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyExtraneousClient : MonoBehaviour 
+{
+	void Start () 
+    {
+	    
+
+	}
+	
+	void Update () 
+    {
+      Client [] clients =   GameObject.FindObjectsOfType<Client>();
+      if (clients.Length > 1)
+          Destroy(clients[1]);
+	}
+}
